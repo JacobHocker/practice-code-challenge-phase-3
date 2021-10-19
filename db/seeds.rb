ec85580743260d890dc721fe392c1adf2fa7681a
JacobHocker <jacobdouglas06@gmail.com>
2021-10-19 08:41:17 -0500

    setting up everything

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+puts "Deleting old data..."
+Product.destroy_all
+User.destroy_all
+Review.destroy_all
+
+puts "Creating users..."
+user1 = 
+user2 = 
+user3 = 
+
+puts "Creating products..."
+product1 = 
+product2 = 
+product3 = 
+product4 = 
+product5 = 
+
+puts "Creating reviews..."
+review1 = 
+review2 = 
+review3 = 
+review4 = 
+review5 = 
+review6 = 
+review7 = 
+review8 = 
+
+binding.pry
